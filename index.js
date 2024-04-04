e68a8a3f588cfcc172ed81eeb8017d527930ef33
vincent <vinnymummo@gmail.com>
2024-04-04 21:46:09 +0300

    commit

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+
+function superbowlWin(record){
+    const win = record.find(entry => entry.result === "W")
+  if (win){
+    return win.year
+  }else {
+    return undefined
+  }
+}
+
+
+
+
+
+// console.log(record.find(superbowlWin).year);
+// function superbowlWin(record){
+//     if (record.result === "W"){ 
+//     return record.result
+//     }else {
+//         return undefined
+//     }
+// }
+// let wYear = superbowlWin()
+// console.log(wYear)
 // code your solution here
